cfa916b28d505a325bd86e5fe5e02f671dbdba97
Amit Singh <np4652@gmail.com>
2023-06-24 13:43:38 +0530

    Add project files.

---
+++ ApiDocs/Controllers/TransController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDocs.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class TransController : ControllerBase
+    {
+        [HttpGet(nameof(Test)+"/{Id}")]
+        public async Task<IActionResult> Test(int Id)
+        {
+            return Ok(Id);
+        }
+    }
+}
