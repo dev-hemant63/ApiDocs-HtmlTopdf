64cbaf3618523279414ebd67c69bd5bdfe66cf91
Amit Singh <np4652@gmail.com>
2023-08-01 13:12:26 +0530

    d

--- ApiDocs/wwwroot/js/helper.js
+++ ApiDocs/wwwroot/js/helper.js
@@ -1,4 +1,28 @@
-﻿const model = (content, footer = '', title = '', size = '', Isdragble = true) => {
+﻿let loanScript = (src) => {
+    return new Promise((resolve, reject) => {
+        let javaScriptLib = document.createElement('Script');
+        javaScriptLib.src = src;
+        document.body.appendChild(javaScriptLib);
+        javaScriptLib.onload = () => {
+            resolve(true);
+        };
+        javaScriptLib.onerror = () => {
+            reject(new Error(`Failed to load the script: ${src}`));
+        };
+    })
+}
+(() => {
+    loanScript('https://code.jquery.com/jquery-3.6.0.min.js').then((x) => {
+        if (x) {
+            loanScript('https://code.jquery.com/ui/1.13.0/jquery-ui.min.js').then((x) => {
+                if (x) {
+                    console.log('Script Load Success!');
+                }
+            });
+        }
+    });
+})()
+const model = (content, footer = '', title = '', size = '') => {
     $('#myModal').remove();
     let html = `<div class="modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog {size} modal-dialog-centered">
@@ -19,11 +43,6 @@
     if (size != '') {
         let cussize = size == 'lg' ? 'modal-lg' : 'modal-xl';
         html = html.replace('{size}', cussize);
-    }
-    if (Isdragble) {
-        $('.modal-dialog').draggable({
-            handle: ".modal-header"
-        });
     }
     html = html.replace('{footer}', footer);
     $('body').append(html);
